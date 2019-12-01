7ba22dcab910b1a65c03a3a1b7b3ec91db03d43e
andrevtribeiro <andre.vitorino.ribeiro@hotmail~.com>
2019-12-01 20:49:09 +0000

    error

--- projeto2paper/n_ring_runs.py
+++ projeto2paper/n_ring_runs.py
@@ -7,13 +7,16 @@ def main():
     neighbors=[2,4,6,8,10,16,24,32]
     robots=[0,10,20,30,40]
     epsilon=[0.001,0.002,0.01,0.02,0.1,0.2]
-    choice=input("press 'n' for neighbors run, 'r' for robots run and 'e' for epsilon run: ")
-    while choice not in ['r','n','e']:
-        choice=input("press 'n' for neighbors run, 'r' for robots run and 'e' for epsilon run: ")
+    population=[100,200,500,1000,2000,4000,8000]
+    choice=input("press 'n' for neighbors run, 'r' for robots run, 'p' for population run and 'e' for epsilon run: ")
+    while choice not in ['r','n','e','p']:
+        choice=input("press 'n' for neighbors run, 'r' for robots run, 'p' for population run and 'e' for epsilon run: ")
     if choice=='n':
         choice=neighbors
     elif choice=='r':
         choice=robots
+    elif choice=='p':
+        choice=population
     else:
         choice=epsilon
     ps=[0 for i in range(len(choice))]
@@ -29,11 +32,13 @@ def main():
         except OSError:
             exit("Could not create a child process")
         if newpid==0:
-            args=[sys.executable,"/mnt/c/Users/andre/OneDrive/Tecnico/CRC/CRC_complex_revol_cooking/projeto2paper/ring_run.py"]
+            args=[sys.executable,"ring_run.py"]
             if choice==robots:
                 args+=["-r",str(choice[i])]
             elif choice==neighbors:
                 args+=["-n",str(choice[i])]
+            elif choice==population:
+                args+=["-p",str(choice[i])]
             else:
                 args+=["-e",str(choice[i])]
             os.execv(sys.executable,args)
@@ -46,11 +51,13 @@ def main():
     for i in range(len(choice)):
         nome_ficheiro='ring_run_'
         if choice==robots:
-            nome_ficheiro+=str(choice[i])+'_robots_4_neighbors_0.01_epsilon'
+            nome_ficheiro+='100_population_'+str(choice[i])+'_robots_4_neighbors_0.001_epsilon'
         elif choice==neighbors:
-            nome_ficheiro+='0_robots_'+str(choice[i])+'_neighbors_0.01_epsilon'
+            nome_ficheiro+='100_population_0_robots_'+str(choice[i])+'_neighbors_0.001_epsilon'
+        elif choice==population:
+            nome_ficheiro+=str(choice[i])+'_population_0_robots_4_neighbors_0.001_epsilon'
         else:
-            nome_ficheiro+='0_robots_4_neighbors_'+str(choice[i])+'_epsilon'
+            nome_ficheiro+='100_population_0_robots_4_neighbors_'+str(choice[i])+'_epsilon'
         f=open(nome_ficheiro,'r')
         t=f.readline().split()
         ps[i]=float(t[0])
--- projeto2paper/ring_run.py
+++ projeto2paper/ring_run.py
@@ -6,6 +6,9 @@ from player import Robot
 from population import Population
 
 def one_run(population, iterations, neighborIterations):
+    m=population.media()
+    lastm=(0,0)
+    n=len(population.population)-population.nrobots
     mediasp=[]
     mediasq=[]
     mediap=0
@@ -13,56 +16,57 @@ def one_run(population, iterations, neighborIterations):
     varp=[100,-1]
     varq=[100,-1]
     nmedia=100000
-    
+    limit=(False,0)
     for t in range(iterations):
+        flag=False
         p1=population.randomPlayer()
         p2=population.randomNeighbor(p1)
         f1=population.getFitness(p1)
         f2=population.getFitness(p2)
-        if f2>f1:
+        if f2>f1 and not p1.isRobot:
+            previous=(p1.p,p1.q)
             population.imitate(p1,p2)
+            flag=True
+        if flag:
+            m=(m[0]-previous[0]+p1.p,m[1]-previous[1]+p1.q)
         if neighborIterations==1:
-            m=population.media()  
-            mediasp+=[m[0]]
-            mediasq+=[m[1]]
-            if t>= (iterations-nmedia):
-                mediap += m[0]
-                mediaq += m[1]
-                varp[0]=min(varp[0],m[0])
-                varp[1]=max(varp[1],m[0])
-                varq[0]=min(varq[0],m[1])
-                varq[1]=max(varq[1],m[1])
-        else:
-            if t>= (iterations-nmedia):
-                m=population.media()  
-                mediap += m[0]
-                mediaq += m[1]
-                varp[0]=min(varp[0],m[0])
-                varp[1]=max(varp[1],m[0])
-                varq[0]=min(varq[0],m[1])
-                varq[1]=max(varq[1],m[1])
-                
+            mediasp+=[m[0]/n]
+            mediasq+=[m[1]/n]
+        if t>=(iterations-nmedia) or limit[0]:
+            mediap += m[0]/n
+            mediaq += m[1]/n
+            varp[0]=min(varp[0],m[0]/n)
+            varp[1]=max(varp[1],m[0]/n)
+            varq[0]=min(varq[0],m[1]/n)
+            varq[1]=max(varq[1],m[1]/n)
+        if t%100000==0 and limit[0]==False:   
+            if abs(lastm[0]-m[0])<1e-1 and abs(lastm[1]-m[1])<1e-1:
+                limit=(True,t+nmedia+1)
+            lastm=m
+        if t==limit[1] and limit[0]:
+            break
+    m2=population.media()
     if neighborIterations==1:
         plt.subplot(2, 1, 1)
-        plt.scatter(list(range(len(mediasp))),mediasp)
+        plt.plot(list(range(len(mediasp))),mediasp)
         plt.title('Ps')
         plt.subplot(2, 1, 2)
-        plt.scatter(list(range(len(mediasq))),mediasq,c='orange')
+        plt.plot(list(range(len(mediasq))),mediasq,c='orange')
         plt.title('Qs')
         plt.show()
     return [[mediap/nmedia,varp[0],varp[1]],[mediaq/nmedia,varq[0],varq[1]]]
 
 def main(argv):
-    n=101
+    n=100
     e=0.001
     iterations=800000
     robots = 0
     neighbors= 4
     neighborIterations=30
     try:
-        opts,args = getopt.getopt(argv,"n:r:i:e:")
+        opts,args = getopt.getopt(argv,"n:r:i:e:p:")
     except getopt.GetoptError:
-        print('test.py -n <neighbors> -r <robots> -i <iterations>')
+        print('test.py -n <neighbors> -r <robots> -i <iterations> -e <epsilon> -p population')
         sys.exit(2)
     for opt, arg in opts:
         if opt=="-n":
@@ -73,12 +77,16 @@ def main(argv):
             neighborIterations=int(arg)
         elif opt=="-e":
             e=float(arg)
+        elif opt=="-p":
+            n=int(arg)
+    iterations*=n/100
+    print(iterations)
     pm=0
     qm=0
     varp=[100,-1]
     varq=[100,-1]
     for i in range(neighborIterations):
-        print(str(robots)+","+str(neighbors)+","+str(e) + "->" + str(i))
+        print(str(n)+","+str(robots)+","+str(neighbors)+","+str(e) + "->" + str(i))
         population=Population(n,e)
         for i in range(robots):
             population.addRobot(random.random(), random.random())
@@ -96,7 +104,7 @@ def main(argv):
             varq[1]=t[1][2]
     pm/=neighborIterations
     qm/=neighborIterations
-    nomeFicheiro='ring_run_'+ str(robots) + "_robots_"+str(neighbors)+"_neighbors_"+str(e)+"_epsilon"
+    nomeFicheiro='ring_run_'+str(n)+'_population_'+str(robots) + "_robots_"+str(neighbors)+"_neighbors_"+str(e)+"_epsilon"
     f=open(nomeFicheiro,'w')
     conteudo=str(pm)+" "+str(qm)+" "+str(varp[0])+" "+str(varp[1])+" "+str(varq[0])+" "+str(varq[1])
     f.write(conteudo)
